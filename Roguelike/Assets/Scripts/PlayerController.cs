2dd138a794b82b42d5904d05e07b0ec6db102a48
dylangilson <dylan.gilson@outlook.com>
2024-07-02 22:09:28 -0700

    implemented player movement, player animation, and overworld collisions

---
+++ Roguelike/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float movementSpeed;
+    public LayerMask solidObjectsLayer;
+    
+    private bool isMoving;
+    private Vector2 input;
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (!isMoving) {
+            // get input
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            // remove diagonal movement
+            if (input.x != 0) {
+                input.y = 0;
+            }
+
+            // move
+            if (input != Vector2.zero) {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                var targetPosition = transform.position;
+                targetPosition.x += input.x;
+                targetPosition.y += input.y;
+
+                if (IsWalkable(targetPosition)) {
+                    StartCoroutine(Move(targetPosition));
+                }
+            }
+        }
+        
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    IEnumerator Move(Vector3 targetPosition) {
+        isMoving = true;
+
+        while ((targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon) {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+
+        isMoving = false;
+    }
+
+    private bool IsWalkable(Vector3 targetPosition) {
+        return Physics2D.OverlapCircle(targetPosition, 0.3f, solidObjectsLayer) == null;
+    }
+}
